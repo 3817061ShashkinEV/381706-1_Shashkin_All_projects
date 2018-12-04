2c87d20857d00db9eb63f9faec1f349695311b5d
Evgeny <evg.shashkin@yandex.ru>
2018-12-04 23:08:37 +0300

    need tests

--- MatrixTest/test_matrix.cpp
+++ MatrixTest/test_matrix.cpp
@@ -1,7 +1,7 @@
 #include <gtest.h>
 #include <Matrix.h>
 
-TEST(Matrix, can_create_matrix_with_positive_size)
+/*TEST(Matrix, can_create_matrix_with_positive_size)
 {
   ASSERT_NO_THROW(TMatrix<TVector<int> > M);
-}
+}*/
