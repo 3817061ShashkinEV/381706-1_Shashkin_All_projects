71c210b3d24853274e263bb79b2b5ba8ad56cdb9
Evgeny <evg.shashkin@yandex.ru>
2018-12-06 02:44:36 +0300

    little changes

--- StackTest/test_stack.cpp
+++ StackTest/test_stack.cpp
@@ -3,70 +3,69 @@
 
 TEST(Stack, can_create_stack_with_positive_size)
 {
-  ASSERT_NO_THROW(TStack<int> S(1));
+  ASSERT_NO_THROW(TStack<int> stack(1));
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, throw_when_create_stack_with_negative_size)
 {
-  ASSERT_ANY_THROW(TStack<int> S(-5));
+  ASSERT_ANY_THROW(TStack<int> stack(-5));
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_create_copied_stack)
 {
-  TStack<int> S1(5);
-  ASSERT_NO_THROW(TStack<int> S2(S1));
+  TStack<int> stack1(5);
+  ASSERT_NO_THROW(TStack<int> stack2(stack1));
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_put_an_element)
 {
-  TStack<int> S(5);
+  TStack<int> stack(5);
   int element = 10;
-  ASSERT_NO_THROW(S.Put(10));
+  ASSERT_NO_THROW(stack.Put(10));
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_get_an_element)
 {
-  TStack<int> S(5);
-  int element = 10;
-  S.Put(element);
-  EXPECT_EQ(element,S.Get());
+  TStack<int> stack(5);
+  stack.Put(10);
+  EXPECT_EQ(10,stack.Get());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_use_isempty_correctly)
 {
-  TStack<int> S(5);
-  ASSERT_TRUE(S.IsEmpty());
+  TStack<int> stack(5);
+  ASSERT_TRUE(stack.IsEmpty());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_use_isempty_incorrectly)
 {
-  TStack<int> S(2);
-  S.Put(10);
-  ASSERT_FALSE(S.IsEmpty());
+  TStack<int> stack(2);
+  stack.Put(10);
+  ASSERT_FALSE(stack.IsEmpty());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_use_isfull_correctly)
 {
-  TStack<int> S(1);
-  S.Put(10);
-  ASSERT_TRUE(S.IsFull());
+  TStack<int> stack(1);
+  stack.Put(10);
+  ASSERT_TRUE(stack.IsFull());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, can_use_isfull_incorrectly)
 {
-  TStack<int> S(5);
-  ASSERT_FALSE(S.IsFull());
+  TStack<int> stack(5);
+  ASSERT_FALSE(stack.IsFull());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, throw_when_get_an_element_but_stack_is_empty)
 {
-  TStack<int> S(1);
-  ASSERT_ANY_THROW(S.Get());
+  TStack<int> stack(1);
+  ASSERT_ANY_THROW(stack.Get());
 }
 // ---------------------------------------------------------------------------
 TEST(Stack, throw_when_put_an_element_but_stack_is_full)
 {
-  TStack<int> S(1);
-  S.Put(10);
-  ASSERT_ANY_THROW(S.Put(5));
+  TStack<int> stack(1);
+  stack.Put(10);
+  ASSERT_ANY_THROW(stack.Put(5));
 }
--- VectorTest/test_vector.cpp
+++ VectorTest/test_vector.cpp
@@ -3,183 +3,183 @@
 
 TEST(Vector, can_create_vector_with_positive_size)
 {
-  ASSERT_NO_THROW(TVector<int> V);
+  ASSERT_NO_THROW(TVector<int> vector);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_create_vector_with_negative_size)
 {
-  ASSERT_ANY_THROW(TVector<int> V(-10));
+  ASSERT_ANY_THROW(TVector<int> vector(-10));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_create_vector_with_negative_startindex)
 {
-  ASSERT_ANY_THROW(TVector<int> V(10,-5));
+  ASSERT_ANY_THROW(TVector<int> vector(10,-5));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_create_vector_with_startindex_large_than_size)
 {
-  ASSERT_ANY_THROW(TVector<int> V(10, 11));
+  ASSERT_ANY_THROW(TVector<int> vector(10, 11));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_create_copied_vector)
 {
-  TVector<int> V(10);
-  ASSERT_NO_THROW(TVector<int> B(V));
+  TVector<int> vector1(10);
+  ASSERT_NO_THROW(TVector<int> vector2(vector1));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_get_size)
 {
-  TVector<int> V(10);
-  EXPECT_EQ(10, V.GetSize());
+  TVector<int> vector(10);
+  EXPECT_EQ(10, vector.GetSize());
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_get_startindex)
 {
-  TVector<int> V(10,5);
-  EXPECT_EQ(5, V.GetStartIndex());
+  TVector<int> vector(10,5);
+  EXPECT_EQ(5, vector.GetStartIndex());
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_return_element_of_vector_with_negative_index)
 {
-  TVector<int> V(10);
-  ASSERT_ANY_THROW(V[-1]);
+  TVector<int> vector(10);
+  ASSERT_ANY_THROW(vector[-1]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_return_element_of_vector_with_index_equal_size)
 {
-  TVector<int> V(10);
-  ASSERT_ANY_THROW(V[10]);
+  TVector<int> vector(10);
+  ASSERT_ANY_THROW(vector[10]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_return_element_of_vector_with_index_large_than_size)
 {
-  TVector<int> V(10);
-  ASSERT_ANY_THROW(V[11]);
+  TVector<int> vector(10);
+  ASSERT_ANY_THROW(vector[11]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_write_and_return_element_via_index_operator)
 {
-  TVector<int> V(3);
-  V[0] = 0;
-  int tmp = V[0] + 2;
+  TVector<int> vector(3);
+  vector[0] = 0;
+  int tmp = vector[0] + 2;
   EXPECT_EQ(2, tmp);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_compare_vectors)
 {
-  TVector<int> V1(10),V2(10);
-  for (int i = 0; i < V1.GetSize(); i++)
+  TVector<int> vector1(10), vector2(10);
+  for (int i = 0; i < vector1.GetSize(); i++)
   {
-    V1[i] = i;
-    V2[i] = i;
+    vector1[i] = i;
+    vector2[i] = i;
   }
-  ASSERT_TRUE(V1 == V2);
+  ASSERT_TRUE(vector1 == vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_compare_vectors_with_one_size_false)
 {
-  TVector<int> V1(10), V2(10);
-  for (int i = 0; i < V1.GetSize(); i++)
+  TVector<int> vector1(10), vector2(10);
+  for (int i = 0; i < vector1.GetSize(); i++)
   {
-    V1[i] = i;
-    V2[i] = i+2;
+    vector1[i] = i;
+    vector2[i] = i+2;
   }
-  ASSERT_FALSE(V1 == V2);
+  ASSERT_FALSE(vector1 == vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_compare_vectors_with_different_size)
 {
-  TVector<int> V1(10), V2(11);
-  ASSERT_ANY_THROW(V1 == V2);
+  TVector<int> vector1(10), vector2(11);
+  ASSERT_ANY_THROW(vector1 == vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_assign_vector)
 {
-  TVector<int> V1(10), V2(11);
-  V1 = V2;
-  ASSERT_TRUE(V1 == V2);
+  TVector<int> vector1(10), vector2(11);
+  vector1 = vector2;
+  ASSERT_TRUE(vector1 == vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_add_scalar_to_vector)
 {
-  TVector<int> V(10);
-  V=V+5;
-  EXPECT_EQ(5, V[6]);
-  EXPECT_EQ(5, V[8]);
-  EXPECT_NE(9, V[0]);
+  TVector<int> vector(10);
+  vector = vector +5;
+  EXPECT_EQ(5, vector[6]);
+  EXPECT_EQ(5, vector[8]);
+  EXPECT_NE(9, vector[0]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_subtract_scalar_from_vector)
 {
-  TVector<int> V(10);
-  V = V - 5;
-  EXPECT_EQ(-5, V[6]);
-  EXPECT_EQ(-5, V[8]);
-  EXPECT_NE(0, V[0]);
+  TVector<int> vector(10);
+  vector = vector - 5;
+  EXPECT_EQ(-5, vector[6]);
+  EXPECT_EQ(-5, vector[8]);
+  EXPECT_NE(0, vector[0]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_multiply_vector_on_scalar)
 {
-  TVector<int> V(10);
-  for (int i = 0; i < V.GetSize(); i++)
-    V[i] = i;
-  V = V * 2;
-  EXPECT_EQ(12, V[6]);
-  EXPECT_EQ(16, V[8]);
-  EXPECT_NE(1, V[0]);
+  TVector<int> vector(10);
+  for (int i = 0; i < vector.GetSize(); i++)
+    vector[i] = i;
+  vector = vector * 2;
+  EXPECT_EQ(12, vector[6]);
+  EXPECT_EQ(16, vector[8]);
+  EXPECT_NE(1, vector[0]);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_add_two_vector_with_equal_size)
 {
-  TVector<int> V1(3), V2(3), tmp(3);
-  for (int i = 0; i < V1.GetSize(); i++)
+  TVector<int> vector1(3), vector2(3), tmp(3);
+  for (int i = 0; i < vector1.GetSize(); i++)
   {
-    V1[i] = i;
-    V2[i] = V2.GetSize() - i;
+    vector1[i] = i;
+    vector2[i] = vector2.GetSize() - i;
   }
-  tmp = V1 + V2;
-  ASSERT_TRUE(tmp == (V1+V2));
+  tmp = vector1 + vector2;
+  ASSERT_TRUE(tmp == (vector1+ vector2));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_add_two_vectors_with_defferent_size)
 {
-  TVector<int> V1(3), V2(4);
-  ASSERT_ANY_THROW(V1 + V2);
+  TVector<int> vector1(3), vector2(4);
+  ASSERT_ANY_THROW(vector1 + vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_substract_two_vector_with_equal_size)
 {
-  TVector<int> V1(3), V2(3), tmp(3);
-  for (int i = 0; i < V1.GetSize(); i++)
+  TVector<int> vector1(3), vector2(3), tmp(3);
+  for (int i = 0; i < vector1.GetSize(); i++)
   {
-    V1[i] = i;
-    V2[i] = V2.GetSize() - i;
+    vector1[i] = i;
+    vector2[i] = vector2.GetSize() - i;
   }
-  tmp = V2 - V1;
-  ASSERT_TRUE(tmp == (V2-V1));
+  tmp = vector2 - vector1;
+  ASSERT_TRUE(tmp == (vector2- vector1));
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_substract_two_vectors_with_defferent_size)
 {
-  TVector<int> V1(3), V2(4);
-  ASSERT_ANY_THROW(V1 - V2);
+  TVector<int> vector1(3), vector2(4);
+  ASSERT_ANY_THROW(vector1 - vector2);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, can_multiply_two_vector_with_equal_size)
 {
-  TVector<int> V1(3), V2(3);
+  TVector<int> vector1(3), vector2(3);
   int tmp;
-  for (int i = 0; i < V1.GetSize(); i++)
+  for (int i = 0; i < vector1.GetSize(); i++)
   {
-    V1[i] = i;
-    V2[i] = V2.GetSize() - i;
+    vector1[i] = i;
+    vector2[i] = vector2.GetSize() - i;
   }
-  tmp = V2 * V1;
+  tmp = vector2 * vector1;
   EXPECT_EQ(4, tmp);
 }
 // ---------------------------------------------------------------------------
 TEST(Vector, throw_when_multiply_two_vectors_with_defferent_size)
 {
-  TVector<int> V1(3), V2(4);
-  ASSERT_ANY_THROW(V1 * V2);
+  TVector<int> vector1(3), vector2(4);
+  ASSERT_ANY_THROW(vector1 * vector2);
 }
