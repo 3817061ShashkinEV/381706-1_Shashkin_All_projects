3dfada046a50ed2ba189a1eba35138eae415b5c1
Evgeny <evg.shashkin@yandex.ru>
2018-12-17 22:21:30 +0300

    Matrix is done

--- MatrixLib/Matrix.h
+++ MatrixLib/Matrix.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-#include <Vector.h>
+#include <../VectorLib/Vector.h>
 #include <../MyExceptionLib/MyException.h>
 
 template <class T>
@@ -8,36 +8,37 @@ class TMatrix : public TVector<TVector<T> >
 {
 public:
   TMatrix(int _size = 10);
-  TMatrix(const TMatrix<T> &obj);
-  TMatrix(const TVector<TVector<T> > &obj);
+  TMatrix(TMatrix<T> &obj);
+  TMatrix(TVector<TVector<T> > &obj);
   bool operator==(const TMatrix<T> &obj);
   TMatrix<T>& operator=(const TMatrix<T> &obj);
   TMatrix<T> operator+(const TMatrix<T> &obj);
   TMatrix<T> operator-(const TMatrix<T> &obj);
   TMatrix<T> operator*(const TMatrix<T> &obj);
+  TMatrix<T> operator/(TMatrix<T> &obj);
 
   template <class T1>
-  friend std::istream& operator >> (std::istream &istr, TMatrix<T> &matrix);
+  friend std::istream& operator >> (std::istream &istr, TMatrix<T1> &matrix);
 
   template <class T1>
-  friend std::ostream& operator << (std::ostream &ostr, TMatrix<T> &matrix);
+  friend std::ostream& operator << (std::ostream &ostr, TMatrix<T1> &matrix);
 };//TMatrix
 // ---------------------------------------------------------------------------
 template <class T>
 TMatrix<T>::TMatrix(int _size) : TVector<TVector<T> > (_size)
 {
-  if (_size > 0)
-    for (int i = 0; i < _size; i++)
-      this->vector[i] = TVector<T>(_size - i, i);
-  else
+  if (_size <= 0)
     throw TMyException("Error! Size must be positive!\n");
+  else
+    for (int i = 0; i < _size; i++)
+      this->vector[i] = TVector<T>(_size - i);
 }
 // ---------------------------------------------------------------------------
 template <class T>
-TMatrix<T>::TMatrix(const TMatrix<T> &obj) : TVector<TVector<T> >(obj) {}
+TMatrix<T>::TMatrix(TMatrix<T> &obj) : TVector<TVector<T> >(obj) {}
 // ---------------------------------------------------------------------------
 template <class T>
-TMatrix<T>::TMatrix(const TVector<TVector<T> > &obj) : TVector<TVector<T> >(obj) {}
+TMatrix<T>::TMatrix(TVector<TVector<T> > &obj) : TVector<TVector<T> >(obj) {}
 // ---------------------------------------------------------------------------
 template <class T>
 bool TMatrix<T>::operator==(const TMatrix<T> &obj)
@@ -77,26 +78,66 @@ TMatrix<T> TMatrix<T>::operator*(const TMatrix<T> &obj)
   if (this->size != obj.size)
     throw TMyException("Error! Matrices have different sizes!\n");
   else
-    return TVector<TVector<T> >::operator*(obj);
+  {
+    TMatrix<T> rez(this->size);
+    for (int i = 0; i < this->size; i++)
+      for (int j = i; j < this->size; j++)
+        for (int k = i; k <= j; k++)
+          rez.vector[i][j - i] += this->vector[i][k - i] * obj.vector[k][j - k];
+    return rez;
+  }
 }
 // ---------------------------------------------------------------------------
 template <class T>
-std::istream& operator >> (std::istream &istr, TMatrix<T> &matrix)
+TMatrix<T> TMatrix<T>::operator/(TMatrix<T> &obj)
+{
+  if (this->size != obj.size)
+    throw TMyException("Error! Matrices have different sizes!\n");
+  double determinant = 1;
+  for (int i = 0; i < this->size; i++)
+    determinant *= this->vector[i][0];
+  if (determinant == 0)
+    throw TMyException("Error! Determinant of the right matrix equals zero!\n");
+  TMatrix<T> copyObj(obj);
+  TMatrix<T> rez(this->size);
+  for (int i = 0; i < this->size; i++)
+  {
+    rez[i][0] = 1;
+    for (int j = 0; j < (this->size - i); j++)
+    {
+      copyObj[i][j] = copyObj[i][j]/copyObj[i][0];
+      rez[i][j] = rez[i][j]/copyObj[i][0];
+    }
+  }
+  for (int i = 1; i < this->size; i++)
+    for (int j = 0; j < i; j++)
+    {
+      T tmp = copyObj[j][i - j];
+      for (int k = i - j, l = 0; k < (this->size - j); k++)
+      {
+        copyObj[j][k] = copyObj[j][k] - copyObj[i][l] * tmp;
+        rez[j][k] = rez[j][k] - rez[i][l++] * tmp;
+      }
+    }
+  return ((*this)*rez);
+}
+// ---------------------------------------------------------------------------
+template <class T1>
+std::istream& operator >> (std::istream &istr, TMatrix<T1> &matrix)
 {
   for (int i = 0; i < matrix.size; i++)
     istr >> matrix.vector[i];
   return istr;
 }
 // ---------------------------------------------------------------------------
-template <class T>
-std::ostream& operator << (std::ostream &ostr, TMatrix<T> &matrix)
+template <class T1>
+std::ostream& operator << (std::ostream &ostr, TMatrix<T1> &matrix)
 {
   for (int i = 0; i < matrix.size; i++)
   {
-    ostr << matrix.vector[i];
-    if (i != matrix.size - 1)
+    for (int j = 1; j <= i; j++)
       ostr << " ";
-    else
+      ostr << matrix.vector[i];
       ostr << "\n";
   }
   return ostr;
--- MatrixTest/test_matrix.cpp
+++ MatrixTest/test_matrix.cpp
@@ -1,7 +1,132 @@
 #include <gtest.h>
 #include <Matrix.h>
 
-/*TEST(Matrix, can_create_matrix_with_positive_size)
+TEST(Matrix, can_create_matrix_with_positive_size)
 {
-  ASSERT_NO_THROW(TMatrix<TVector<int> > M);
-}*/
+  ASSERT_NO_THROW(TMatrix<int> matrix(5));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, throw_when_create_matrix_with_negative_size)
+{
+  ASSERT_ANY_THROW(TMatrix<TVector<int> >matrix(-5));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_create_copied_matrix)
+{
+  TMatrix<int> matrix1(5);
+  ASSERT_NO_THROW(TMatrix<int> matrix2(matrix1));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_compare_matrices)
+{
+  TMatrix<int> matrix1(5), matrix2(5);
+  for(int i=0;i<matrix1.GetSize();i++)
+    for (int j = 0; j < matrix1.GetSize()-i; j++)
+    {
+      matrix1[i][j] = j;
+      matrix2[i][j] = j;
+    }
+  ASSERT_TRUE(matrix1 == matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_compare_matrices_with_one_size_false)
+{
+  TMatrix<int> matrix1(5), matrix2(5);
+  for (int i = 0; i < matrix1.GetSize(); i++)
+    for (int j = 0; j < matrix1.GetSize() - i; j++)
+    {
+      matrix1[i][j] = i;
+      matrix2[i][j] = j;
+    }
+  ASSERT_FALSE(matrix1 == matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_compare_compare_matrices_with_different_size_false)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  ASSERT_FALSE(matrix1 == matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_assign_matrix)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  matrix1 = matrix2;
+  ASSERT_TRUE(matrix1 == matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_add_matrices_with_equal_size)
+{
+  TMatrix<int> matrix1(5), matrix2(5),tmp(5);
+  for (int i = 0; i < matrix1.GetSize(); i++)
+    for(int j=0; j < matrix1.GetSize()-i; j++)
+    {
+    matrix1[i][j] = i;
+    matrix2[i][j] = j;
+    }
+  tmp = matrix1 + matrix2;
+  ASSERT_TRUE(tmp == (matrix1 + matrix2));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, throw_when_add_matrices_with_different_size)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  ASSERT_ANY_THROW(matrix1 + matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_substract_matrices_with_equal_size)
+{
+  TMatrix<int> matrix1(5), matrix2(5), tmp(5);
+  for (int i = 0; i < matrix1.GetSize(); i++)
+    for (int j = 0; j < matrix1.GetSize() - i; j++)
+    {
+      matrix1[i][j] = i;
+      matrix2[i][j] = j;
+    }
+  tmp = matrix1 - matrix2;
+  ASSERT_TRUE(tmp == (matrix1 - matrix2));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, throw_when_substract_matrices_with_different_size)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  ASSERT_ANY_THROW(matrix1 - matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_multiply_matrices_with_equal_size)
+{
+  TMatrix<int> matrix1(5), matrix2(5), tmp(5);
+  for (int i = 0; i < matrix1.GetSize(); i++)
+    for (int j = 0; j < matrix1.GetSize() - i; j++)
+    {
+      matrix1[i][j] = i;
+      matrix2[i][j] = j;
+    }
+  tmp = matrix1 * matrix2;
+  ASSERT_TRUE(tmp == (matrix1 * matrix2));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, throw_when_multiply_matrices_with_different_size)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  ASSERT_ANY_THROW(matrix1 * matrix2);
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, can_divide_matrices_with_equal_size)
+{
+  TMatrix<int> matrix1(3), matrix2(3), tmp(3);
+  for (int i = 0; i < matrix1.GetSize(); i++)
+    for (int j = 0; j < matrix1.GetSize() - i; j++)
+    {
+      matrix1[i][j] = i + 1;
+      matrix2[i][j] = j + 2;
+    }
+  tmp = matrix1 / matrix2;
+  ASSERT_TRUE(tmp == (matrix1 / matrix2));
+}
+// ---------------------------------------------------------------------------
+TEST(Matrix, throw_when_divide_matrices_with_different_size)
+{
+  TMatrix<int> matrix1(5), matrix2(6);
+  ASSERT_ANY_THROW(matrix1 / matrix2);
+}
+
