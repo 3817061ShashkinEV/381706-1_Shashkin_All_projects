a8bbf402ada3adae28705fabe1b6dc212f785143
Evgeny <evg.shashkin@yandex.ru>
2018-12-18 22:44:29 +0300

    Stack is done

--- Stack/stack_main.cpp
+++ Stack/stack_main.cpp
@@ -12,8 +12,9 @@ int main()
     std::cout << "full.\n";
   else
     std::cout << "not empty and not full.\n";
+  std::cout << "Filling the stack with values from 1 to 10... \n";
   for (int i = 0; i < 10; i++)
-    stack.Put(i);
+    stack.Put(i+1);
   std::cout << "Stack is: ";
   if (stack.IsEmpty())
     std::cout << "empty.\n";
@@ -21,6 +22,7 @@ int main()
     std::cout << "full.\n";
   else
     std::cout << "not empty and not full.\n";
+  std::cout << "Getting element from stack...\n";
   tmp = stack.Get();
   std::cout << "Element from stack equals: " << tmp << "\n";
   std::cout << "Stack is: ";
